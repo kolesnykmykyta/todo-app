e9ee5561be38d2cb64f6585a1c08d2d5342ff150
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-03-14 18:15:47 +0200

    Implement new task adding functionality. Update Index

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Data.Entities;
 using Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
@@ -19,6 +20,26 @@ namespace WebApp.Controllers
         public IActionResult Index()
         {
             return View(_mapper.Map<IEnumerable<TaskModel>>(_taskRepository.GetAll()));
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new TaskModel());
+        }
+
+        [HttpPost]
+        public IActionResult Add(TaskModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                _taskRepository.AddAsync(_mapper.Map<TodoTask>(task));
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(task);
+            }
         }
     }
 }
