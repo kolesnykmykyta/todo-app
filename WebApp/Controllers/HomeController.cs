353096b2ab82985b894a5f5da7e5ac3044629a9c
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-03-14 17:13:10 +0200

    Update Index view and method to use the TaskModel instead of TodoTask

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
-﻿using Data.Repositories;
+﻿using AutoMapper;
+using Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ITodoTaskRepository _taskRepository;
+        private readonly IMapper _mapper;
 
-        public HomeController(ITodoTaskRepository taskRepository)
+        public HomeController(ITodoTaskRepository taskRepository, IMapper mapper)
         {
             _taskRepository = taskRepository;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            return View(_taskRepository.GetAll());
+            return View(_mapper.Map<IEnumerable<TaskModel>>(_taskRepository.GetAll()));
         }
     }
 }
